f187f97359f52b1df862e0b913dc137373fe214a
Jason Thrasher <jasonthrasher@gmail.com>
2018-11-16 13:32:57 -0800

    created render script

---
+++ render.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+#
+# Script to render STLs from command line, with thread per part rendering
+# author: Jason Thrasher
+
+import subprocess
+import math
+import time
+
+# render the stl
+def render():
+	# render each part in a thread, so it all goes faster
+	print 'Starting render'
+	start = time.time()
+
+	scad = []
+	scad.append(subprocess.Popen(['openscad','-D', 'foo=bar','raspberry-pi-3-model-a-plus.scad','-o','raspberry-pi-3-model-a-plus.stl']))
+	# wait for all threads to finish, so we know we're done
+	for p in scad:
+		p.wait()
+
+	elapsed = round(time.time() - start, 1)
+	print 'Rendering done in', elapsed, 'seconds!'
+
+def main():
+	render()
+
+main()
